96c5d338a35e358e37f1c376da195d037ea533f0
Ganapati <ganapatasakatwa@gmail.com>
2023-05-29 19:04:07 +0530

    [GK] using Float as Return Type

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -7,14 +7,19 @@ namespace Generics
         {
             while(true)
             {
-                Console.WriteLine("Enter the Choice\n1)Int Data Type");
+                Console.WriteLine("Enter the Choice\n1)Int Data Type\n2)Float Data Type");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch(choice)
                 {
                     case 1:
                         IntDataType intDataType = new IntDataType();
-                        int result = intDataType.IntCompare(4, 3, 2 );
-                            Console.WriteLine(result);
+                        int result1 = intDataType.IntCompare(4, 3, 2 );
+                            Console.WriteLine(result1);
+                        break;
+                    case 2:
+                        FloatDataType floatDataType = new FloatDataType();
+                        float result2=floatDataType.FloatCompare(4.3f, 2.5f, 5.6f );
+                        Console.WriteLine(result2);
                         break;
                 }
             }
