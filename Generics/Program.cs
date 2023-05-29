fa9c8188246cb79ade0bf49777d511a01fcfdc38
Ganapati <ganapatasakatwa@gmail.com>
2023-05-29 18:54:16 +0530

    [GK] Using int as Return Type

---
+++ Generics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Generics
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Enter the Choice\n1)Int Data Type");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        IntDataType intDataType = new IntDataType();
+                        int result = intDataType.IntCompare(4, 3, 2 );
+                            Console.WriteLine(result);
+                        break;
+                }
+            }
+        }
+    }
+}
