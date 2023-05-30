f1c682e340902da301c4d192fc45ddd0624f5b2b
Ganapati <ganapatasakatwa@gmail.com>
2023-05-30 06:01:55 +0530

    [GK] Using String Data Type

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -7,7 +7,7 @@ namespace Generics
         {
             while(true)
             {
-                Console.WriteLine("Enter the Choice\n1)Int Data Type\n2)Float Data Type");
+                Console.WriteLine("Enter the Choice\n1)Int Data Type\n2)Float Data Type\n3)String Data Type");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch(choice)
                 {
@@ -20,6 +20,11 @@ namespace Generics
                         FloatDataType floatDataType = new FloatDataType();
                         float result2=floatDataType.FloatCompare(4.3f, 2.5f, 5.6f );
                         Console.WriteLine(result2);
+                        break;
+                    case 3:
+                        StringDataType stringDataType = new StringDataType();
+                        string result3 = stringDataType.StringCompare("1", "4","3");
+                        Console.WriteLine(result3);
                         break;
                 }
             }
---
+++ Generics/StringDataType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class StringDataType
+    {
+        public string StringCompare(string first, string second, string third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+                return first;
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+                return second;
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+                return third;
+            return default;
+        }
+    }
+}
