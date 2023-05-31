2850cc8c78fd375f90f8a70acd62175605e109fe
Ganapati <ganapatasakatwa@gmail.com>
2023-05-31 06:43:37 +0530

    [GK] Printing Maximum Array Using Generics Concept

---
+++ Generics/PrintMax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class PrintMax<T> where T : IComparable
+    {
+        public T[] value;
+        public PrintMax(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] SortValue(T[] value)
+        {
+            Array.Sort(value);
+            return value;
+        }
+        public T FindMax(T[] values)
+        {
+            var sortedValues= SortValue(this.value);
+            return sortedValues[sortedValues.Length - 1];
+        }
+        public T FindMaxMethod()
+        {
+            var maxValue= FindMax(this.value);
+            return maxValue;
+        }
+        public void Printmax()
+        {
+            var MaxValue= FindMax(this.value);
+            Console.WriteLine("Maximum Value--"+MaxValue);
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -8,7 +8,7 @@ namespace Generics
             while(true)
             {
                 Console.WriteLine("Enter the Choice\n1)Int Data Type\n2)Float Data Type\n3)String Data Type"+
-                    "\n4)Generic Method\n5)Generic Class\n6)More then Three Parameters");
+                    "\n4)Generic Method\n5)Generic Class\n6)More then Three Parameters\n7)Print Max Array");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch(choice)
                 {
@@ -41,6 +41,11 @@ namespace Generics
                         MoreThenTherees<char> moreThenTherees = new MoreThenTherees<char>('a', 'B','C', 'e');
                         char result6 = moreThenTherees.GenericCompare();
                         Console.WriteLine(result6);
+                        break;
+                    case 7:
+                        int[] intArray = { 1, 3, 5, 8 };
+                        PrintMax<int> printMax = new PrintMax<int>(intArray);
+                        printMax.Printmax();
                         break;
                 }
             }
